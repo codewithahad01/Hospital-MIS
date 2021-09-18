05548871b35ecdbb8923ead687c281ba84436ee0
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2021-09-18 19:50:42 +0430

    contact section created and added some static contacts

---
+++ contact.php
@@ -0,0 +1,33 @@
+<?php require_once("connection.php"); ?>
+
+<?php require_once("top.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style >
+        
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    
+    
+    
+    <h3 style="margin-left: 450px; padding-top: 100px;">hospit@hotmail.com</h3>
+    <h3 style="margin-left: 500px;">0796945070</h3>
+    
+
+    
+
+</body>
+</html>
+
+<?php require_once("footer.php"); ?>
+
+
+
+
