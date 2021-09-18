fc0be50887671542c038685f32c0c83abc2cd0e9
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2021-09-18 19:52:48 +0430

    footer andded and linked in every pages

---
+++ footer.php
@@ -0,0 +1,11 @@
+    </div>
+
+    <div id="footer" class="noprint">
+        Copyright &copy;<?php  echo date("Y");  ?> All Rights Reserved.<br>
+        Powered by: Abdulahad Ahmadi
+    </div>
+
+</div>
+
+</body>
+</html>
